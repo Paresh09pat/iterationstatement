1ced5f7721bc2568da1d127742c804b86570cfb4
Paresh <patilpareshbaluchopda@gmail.com>
2022-07-05 11:32:54 +0530

    swami

---
+++ findfives.js
@@ -0,0 +1,20 @@
+// Find the Fives.
+// Among all the digits from 0−9, PrepBuddy likes number 5. He has a number and wants you to find out the number of times 5 occurred in the given number.
+// Note: You have to complete Find_Five. No need to take any input.
+
+
+const Find_Five = (N) => 
+{
+      let count=0;
+      while(N !== 0){
+     let remainder= N % 10;
+    N = parseInt(N / 10);
+    if (remainder == 5){
+          count = count + 1;
+    }
+    
+  }
+  return (count);
+};
+
+
---
+++ printodd.js
@@ -0,0 +1,15 @@
+// Print the Odds.
+// Write a program which takes  a number from user and print all odd numbers in between 2 and N, but print 2 first.
+
+// Note: You have to complete Print_Odd. No need to take any input.
+
+
+const Print_Odd = (N) => 
+{
+	 console.log('2');
+   let num = 3;
+   while(num <= N){
+   console.log(num);
+   num+=2;
+  }
+};
